f664a8729762c543229f9c311660a2f06ca67759
Kulikov <dima.kulikov1993@gmail.com>
2024-04-17 17:31:33 +0300

    добавлена сущность Компания

--- ActivitySeeker.Domain/Activity.cs
+++ ActivitySeeker.Domain/Activity.cs
@@ -5,4 +5,6 @@ public class Activity
     public Guid Id { get; set; }
 
     public string Name { get; set; } = default!;
+
+    public string Company { get; set; } = default!;
 }
---
+++ ActivitySeeker.Domain/Company.cs
@@ -0,0 +1,5 @@
+namespace ActivitySeeker.Domain;
+
+public class Company
+{
+}
