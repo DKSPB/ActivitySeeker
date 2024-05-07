5cc9b39f12a6f780e40db9da1a84882b11822dde
Dmitry <dima.kulikov1993@gmail.com>
2024-05-07 16:37:52 +0300

    test

---
+++ ActivitySeeker.Bll/Models/ActivityRequest.cs
@@ -0,0 +1,18 @@
+﻿using ActivitySeeker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitySeeker.Bll.Models
+{
+    internal class ActivityRequest
+    {
+        public Guid? Category { get; set; }
+
+        public DateTime? SearchFrom { get; set; }
+
+        public DateTime? SearchTo { get; set;}
+    }
+}
---
+++ ActivitySeeker.Bll/Services/ActivityService.cs
@@ -0,0 +1,27 @@
+﻿using ActivitySeeker.Bll.Models;
+using ActivitySeeker.Domain;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitySeeker.Bll.Services
+{
+    internal class ActivityService
+    {
+        private readonly ActivitySeekerContext _context;
+
+        public ActivityService(ActivitySeekerContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Activity> GetActivities(ActivityRequest requestParams)
+        {
+
+            return null;
+        }
+    }
+}
