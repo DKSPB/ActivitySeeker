5aa50759ba907dc353af5611f95e76081bd7065a
Kulikov <dima.kulikov1993@gmail.com>
2024-09-26 10:05:52 +0300

    Добавление Nlog

--- ActivitySeeker.Api/Program.cs
+++ ActivitySeeker.Api/Program.cs
@@ -5,7 +5,8 @@ using ActivitySeeker.Bll.Interfaces;
 using ActivitySeeker.Bll.Services;
 using ActivitySeeker.Domain;
 using FluentValidation;
-
+using NLog;
+using NLog.Web;
 using Telegram.Bot;
 using Microsoft.AspNetCore.HttpOverrides;
 
@@ -15,80 +16,95 @@ namespace ActivitySeeker.Api
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+            logger.Info("init main");
 
-            var botConfigurationSection = builder.Configuration.GetSection(BotConfiguration.Configuration);
-            builder.Services.Configure<BotConfiguration>(botConfigurationSection);
-            
-            var botConfiguration = botConfigurationSection.Get<BotConfiguration>();
-            
-            var connection = builder.Configuration.GetConnectionString("ActivitySeekerConnection");
-            builder.Services.AddDbContext<ActivitySeekerContext>(options => options.UseNpgsql(connection));
-            builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
-            builder.Services.AddScoped<ActivitySeekerContext>();
-            builder.Services.AddScoped<IUserService, UserService>();
-            builder.Services.AddScoped<IActivityTypeService, ActivityTypeService>();
-            builder.Services.AddScoped<IActivityService, ActivityService>();
-            builder.Services.AddScoped<StartHandler>();
-            builder.Services.AddScoped<MainMenuHandler>();
-            builder.Services.AddScoped<ListOfActivitiesHandler>();
-            builder.Services.AddScoped<SelectActivityTypeHandler>();
-            builder.Services.AddScoped<SelectActivityPeriodHandler>();
-            builder.Services.AddScoped<SelectTodayPeriodHandler>();
-            builder.Services.AddScoped<SelectTomorrowPeriodHandler>();
-            builder.Services.AddScoped<SelectAfterTomorrowPeriodHandler>();
-            builder.Services.AddScoped<SelectWeekPeriodHandler>();
-            builder.Services.AddScoped<SelectMonthPeriodHandler>();
-            builder.Services.AddScoped<SelectUserPeriodHandler>();
-            builder.Services.AddScoped<UserSetFromDateHandler>();
-            builder.Services.AddScoped<UserSetByDateHandler>();
-            builder.Services.AddScoped<SearchResultHandler>();
-            builder.Services.AddScoped<PreviousHandler>();
-            builder.Services.AddScoped<NextHandler>();
-            
-            builder.Services.AddHttpClient("telegram_bot_client").AddTypedClient<ITelegramBotClient>(httpClient =>
+            try
             {
-                TelegramBotClientOptions options = new(botConfiguration.BotToken);
-                return new TelegramBotClient(options, httpClient);
-            });
-            
-            builder.Services.AddHttpClient();
-            builder.Services.AddHostedService<ConfigureWebhook>();
+                var builder = WebApplication.CreateBuilder(args);
 
-            #region serialize settings
+                builder.Logging.ClearProviders();
+                builder.Host.UseNLog();
 
-            builder.Services.AddControllers().AddNewtonsoftJson();
+                var botConfigurationSection = builder.Configuration.GetSection(BotConfiguration.Configuration);
+                builder.Services.Configure<BotConfiguration>(botConfigurationSection);
 
-            #endregion
+                var botConfiguration = botConfigurationSection.Get<BotConfiguration>();
 
-            builder.Services.AddSwaggerGen();
+                var connection = builder.Configuration.GetConnectionString("ActivitySeekerConnection");
+                builder.Services.AddDbContext<ActivitySeekerContext>(options => options.UseNpgsql(connection));
+                builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+                builder.Services.AddScoped<ActivitySeekerContext>();
+                builder.Services.AddScoped<IUserService, UserService>();
+                builder.Services.AddScoped<IActivityTypeService, ActivityTypeService>();
+                builder.Services.AddScoped<IActivityService, ActivityService>();
+                builder.Services.AddScoped<StartHandler>();
+                builder.Services.AddScoped<MainMenuHandler>();
+                builder.Services.AddScoped<ListOfActivitiesHandler>();
+                builder.Services.AddScoped<SelectActivityTypeHandler>();
+                builder.Services.AddScoped<SelectActivityPeriodHandler>();
+                builder.Services.AddScoped<SelectTodayPeriodHandler>();
+                builder.Services.AddScoped<SelectTomorrowPeriodHandler>();
+                builder.Services.AddScoped<SelectAfterTomorrowPeriodHandler>();
+                builder.Services.AddScoped<SelectWeekPeriodHandler>();
+                builder.Services.AddScoped<SelectMonthPeriodHandler>();
+                builder.Services.AddScoped<SelectUserPeriodHandler>();
+                builder.Services.AddScoped<UserSetFromDateHandler>();
+                builder.Services.AddScoped<UserSetByDateHandler>();
+                builder.Services.AddScoped<SearchResultHandler>();
+                builder.Services.AddScoped<PreviousHandler>();
+                builder.Services.AddScoped<NextHandler>();
 
-            var app = builder.Build();
-
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
-
-            app.UseExceptionHandler("/ErrorHandling/ProcessError");
-            
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI(options =>
+                builder.Services.AddHttpClient("telegram_bot_client").AddTypedClient<ITelegramBotClient>(httpClient =>
                 {
-                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    TelegramBotClientOptions options = new(botConfiguration.BotToken);
+                    return new TelegramBotClient(options, httpClient);
                 });
+
+                builder.Services.AddHttpClient();
+                builder.Services.AddHostedService<ConfigureWebhook>();
+
+                #region serialize settings
+
+                builder.Services.AddControllers().AddNewtonsoftJson();
+
+                #endregion
+
+                builder.Services.AddSwaggerGen();
+                
+                builder.Logging.ClearProviders();
+                builder.Host.UseNLog();
+
+                var app = builder.Build();
+
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+
+                app.UseExceptionHandler("/ErrorHandling/ProcessError");
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); });
+                }
+
+                app.UseRouting();
+                app.UseAuthorization();
+                app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+                app.Run();
             }
-
-            app.UseRouting();
-            app.UseAuthorization();
-            app.UseEndpoints(endpoints =>
+            catch (Exception exception)
             {
-                endpoints.MapControllers();
-            });
-
-            app.Run();
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
     }
     
