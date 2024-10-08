8ca714f058017352cb2361dd471164026445d2a6
Kulikov <dima.kulikov1993@gmail.com>
2024-10-08 17:48:35 +0300

    Убран закомментированный код. Возвращён ActivityTypeId

--- ActivitySeeker.Bll/Services/UserService.cs
+++ ActivitySeeker.Bll/Services/UserService.cs
@@ -25,6 +25,7 @@ public class UserService: IUserService
         userExists.ChatId = user.ChatId;
         userExists.UserName = user.UserName;
         userExists.ActivityResult = JsonConvert.SerializeObject(user.ActivityResult);
+        userExists.ActivityTypeId = user.State.ActivityType.Id;
         userExists.SearchFrom = user.State.SearchFrom.GetValueOrDefault();
         userExists.SearchTo = user.State.SearchTo.GetValueOrDefault();
 
@@ -44,7 +45,7 @@ public class UserService: IUserService
     /// <inheritdoc />
     public UserDto? GetUserById(long id)
     {
-        var user = _context.Users.FirstOrDefault(x=>x.Id == id);
+        var user = _context.Users.Include(x => x.ActivityType).FirstOrDefault(x=>x.Id == id);
         
         return user is null ? null : new UserDto(user);
     }
