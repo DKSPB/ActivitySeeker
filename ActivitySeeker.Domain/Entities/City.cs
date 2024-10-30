f7a8a407e92c1085b64873b7abd281b3861edea5
Kulikov <dima.kulikov1993@gmail.com>
2024-10-30 17:58:05 +0300

    Начата работа по добавлению локации для активностей

---
+++ ActivitySeeker.Domain/Entities/City.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ActivitySeeker.Domain.Entities;
+
+[Table("city", Schema = "activity_seeker")]
+public class City
+{
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    [Column("name")]
+    public string Name { get; set; } = string.Empty;
+}
