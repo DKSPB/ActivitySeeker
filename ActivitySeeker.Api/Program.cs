277e1f21c18f1aad32ed6f7cdb28062e98dc35b9
Kulikov <dima.kulikov1993@gmail.com>
2025-01-14 17:52:32 +0300

    Начата работа над переработкой структуры файла конфигурации

--- ActivitySeeker.Api/Program.cs
+++ ActivitySeeker.Api/Program.cs
@@ -209,6 +209,26 @@ namespace ActivitySeeker.Api
 
         public string TelegramChannel { get; set; } = default!;
     }
+
+    public class Settings
+    {
+        public TelegramBotSettings TelegramBotSettings { get; set; }
+        
+        public VkBotSettings VkBotSettings { get; set; }
+    }
+
+    public class BotSettings
+    {
+        public string MainMenuImageName { get; set; }
+        
+        public string OfferMenuImageName { get; set; }
+    }
+    
+    public class TelegramBotSettings : BotSettings
+    { }
+
+    public class VkBotSettings : BotSettings
+    { }
 }
 
 
