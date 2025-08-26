c60ead76e970fcc7e58ae78da70aae0ec5c25c9f
Kulikov <dima.kulikov1993@gmail.com>
2025-08-26 15:14:50 +0300

    Rename unpublish command

---
+++ UseCases/Activity/Commands/Unpublish/UnPublishActivityHandler.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.Common;
+
+namespace UseCases.Activity.Commands.UnPublish
+{
+    internal class UnPublishActivityHandler : IRequestHandler<UnPublishActivityCommand>
+    {
+        private readonly IDbContext _context;
+        public UnPublishActivityHandler(IDbContext context)
+        {
+            _context = context;
+        }
+        public async Task Handle(UnPublishActivityCommand request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Activities.FindAsync(new object?[] { request.Id }, cancellationToken) ??
+                throw new ObjectNotFoundException(nameof(Domain.Entities.Activity), request.Id);
+
+            entity.UnpublishActivity();
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+        }
+    }
+}
